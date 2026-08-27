5cc2390fb4ef85dc742eb0056405bc33692603a8
cyw0927 <dkrakaksakst@gmail.com>
2026-08-27 14:44:54 +0900

    Move database settings into core config

--- server/app/db/database.py
+++ server/app/db/database.py
@@ -1,15 +1,7 @@
-import os
-
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL is not set. Create a server/.env file first.")
+from app.core.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
