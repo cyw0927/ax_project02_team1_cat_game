f330cf42b3bcec5404480a816041ae616a9a043b
cyw0927 <dkrakaksakst@gmail.com>
2026-08-27 15:30:54 +0900

    Register cat routes

--- server/app/main.py
+++ server/app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 
+from app.cats.router import router as cats_router
 from app.economy.router import router as economy_router
 from app.learning.router import router as learning_router
 
 app = FastAPI(title="Programming Learning Cat Game API")
 app.include_router(learning_router)
 app.include_router(economy_router)
+app.include_router(cats_router)
 
 
 @app.get("/")
