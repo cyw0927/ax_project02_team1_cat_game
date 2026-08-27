4d85e15f9b277bdda2c6fb2809fff5157e7c3ac1
cyw0927 <dkrakaksakst@gmail.com>
2026-08-27 15:31:58 +0900

    Register housing router

--- server/app/main.py
+++ server/app/main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 
 from app.cats.router import router as cats_router
 from app.economy.router import router as economy_router
+from app.housing.router import router as housing_router
 from app.learning.router import router as learning_router
 
 app = FastAPI(title="Programming Learning Cat Game API")
 app.include_router(learning_router)
 app.include_router(economy_router)
 app.include_router(cats_router)
+app.include_router(housing_router)
 
 
 @app.get("/")
