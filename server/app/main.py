90eff95293b135729a782eef74d850b8be2c1818
cyw0927 <dkrakaksakst@gmail.com>
2026-08-27 15:27:25 +0900

    Register economy router

--- server/app/main.py
+++ server/app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 
+from app.economy.router import router as economy_router
 from app.learning.router import router as learning_router
 
 app = FastAPI(title="Programming Learning Cat Game API")
 app.include_router(learning_router)
+app.include_router(economy_router)
 
 
 @app.get("/")
