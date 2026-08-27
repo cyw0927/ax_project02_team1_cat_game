1167a41ca77aa9e083c8352b9bdf2c9769d379cd
cyw0927 <dkrakaksakst@gmail.com>
2026-08-27 15:33:39 +0900

    Connect ranking router

--- server/app/main.py
+++ server/app/main.py
@@ -4,12 +4,14 @@ from app.cats.router import router as cats_router
 from app.economy.router import router as economy_router
 from app.housing.router import router as housing_router
 from app.learning.router import router as learning_router
+from app.ranking.router import router as ranking_router
 
 app = FastAPI(title="Programming Learning Cat Game API")
 app.include_router(learning_router)
 app.include_router(economy_router)
 app.include_router(cats_router)
 app.include_router(housing_router)
+app.include_router(ranking_router)
 
 
 @app.get("/")
