b81f61e1d9c5a6410e83bef7119be4f2c86452f1
cyw0927 <dkrakaksakst@gmail.com>
2026-08-27 15:18:30 +0900

    Register learning task router

--- server/app/main.py
+++ server/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 
+from app.learning.router import router as learning_router
+
 app = FastAPI(title="Programming Learning Cat Game API")
+app.include_router(learning_router)
 
 
 @app.get("/")
